3dbfacde6eda8e8169a45d616c845b06d93e7819
unknown <mcalumgannon@gmail.com>
2023-11-10 19:45:05 +0000

    Puts spaces inbetween camel case

---
+++ kyu6/Break camelCase/comments.py
@@ -0,0 +1,13 @@
+# define a function to break up camel casing by adding spaces between words.
+
+# initialize an empty string to store result.
+
+# iterate through each character in input string.
+
+# check if character is uppercase and not first character.
+
+# if its an uppercase character and notfirst character, add space before uppercase character.
+
+# orwise append character to result string.
+
+# return result with camel casing broken up by spaces.
